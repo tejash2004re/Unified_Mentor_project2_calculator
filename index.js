ea5508977a90ab05d0dc74afdec2a99f678d064b
TEJASHWIN S <144519865+tejash2004re@users.noreply.github.com>
2024-06-14 21:34:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-        
-        // Function that display value 
-        function dis(val) { 
-            document.getElementById("result").value += val 
-        } 
-  
-        function myFunction(event) { 
-            if (event.key == '0' || event.key == '1' 
-                || event.key == '2' || event.key == '3' 
-                || event.key == '4' || event.key == '5' 
-                || event.key == '6' || event.key == '7' 
-                || event.key == '8' || event.key == '9' 
-                || event.key == '+' || event.key == '-' 
-                || event.key == '*' || event.key == '/') 
-                document.getElementById("result").value += event.key; 
-        } 
-  
-        var cal = document.getElementById("calcu"); 
-        cal.onkeyup = function (event) { 
-            if (event.keyCode === 13) { 
-                console.log("Enter"); 
-                let x = document.getElementById("result").value 
-                console.log(x); 
-                solve(); 
-            } 
-        } 
-  
-        // Function that evaluates the digit and return result 
-        function solve() { 
-            let x = document.getElementById("result").value 
-            let y = math.evaluate(x) 
-            document.getElementById("result").value = y 
-        } 
-  
-        // Function that clear the display 
-        function clr() { 
-            document.getElementById("result").value = "" 
-        }
+function Solve(val) {
+   var v = document.getElementById('res');
+   v.value += val;
+}
+function Result() {
+   var num1 = document.getElementById('res').value;
+   var num2 = eval(num1);
+   document.getElementById('res').value = num2;
+}
+function Clear() {
+   var inp = document.getElementById('res');
+   inp.value = '';
+}
+function Back() {
+   var ev = document.getElementById('res');
+   ev.value = ev.value.slice(0,-1);
+}
